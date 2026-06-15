65d5cc7cd039b6494d2899a37d343c7612a2a5b6
nlemoi3 <nlemoi3@lsu.edu>
2026-06-15 11:04:37 -0500

    Test boats route

--- app/boats/page.tsx
+++ app/boats/page.tsx
@@ -1,26 +1,7 @@
-import { supabase } from "../../lib/supabase";
-
-export default async function BoatsPage() {
-  const { data: boats, error } = await supabase
-    .from("boats")
-    .select("*")
-    .order("name");
-
+export default function BoatsPage() {
   return (
-    <main style={{ padding: "40px" }}>
-      <h1>Boats</h1>
-
-      {error && (
-        <p style={{ color: "red" }}>
-          Error loading boats: {error.message}
-        </p>
-      )}
-
-      <ul>
-        {boats?.map((boat) => (
-          <li key={boat.id}>{boat.name}</li>
-        ))}
-      </ul>
+    <main>
+      <h1>Boats Page Works!</h1>
     </main>
   );
 }
