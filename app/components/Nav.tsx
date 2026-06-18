dbfbdcbd237743471e8ce0e81159de432ebc3858
nlemoi3 <nlemoi3@lsu.edu>
2026-06-18 08:54:19 -0500

    Admin update link

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -25,8 +25,8 @@ export default function Nav() {
 
       <span style={{ marginLeft: "25px", marginRight: "10px" }}>| Admin:</span>
       <Link href="/admin/catch-entry" style={{ marginRight: "15px" }}>Enter Catch</Link>
-      <Link href="/boats" style={{ marginRight: "15px" }}>Boats</Link>
-      <Link href="/anglers" style={{ marginRight: "15px" }}>Anglers</Link>
+      <Link href="/admin/boats" style={{ marginRight: "15px" }}>Boats</Link>
+      <Link href="/admin/anglers" style={{ marginRight: "15px" }}>Anglers</Link>
       <Link href="/admin/events" style={{ marginRight: "15px" }}>Events</Link>
       <Link href="/admin/catches" style={{ marginRight: "15px" }}>Manage Catches</Link>
     </nav>
